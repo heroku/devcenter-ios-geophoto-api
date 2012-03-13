110938c289f5d19148b348381169f6690ab4e707
Mattt Thompson <m@mattt.me>
2012-03-13 11:18:35 -0700

    Removing unnecessary .as_json

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
     lat, lng = params[:lat], params[:lng]
     if lat and lng
       @photos = Photo.nearby(lat.to_f, lng.to_f)
-      respond_with({:photos => @photos}.as_json)
+      respond_with({:photos => @photos})
     else
       respond_with({:message => "Invalid or missing lat/lng parameters"}, :status => 406)
     end
